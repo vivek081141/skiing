f46505f34ce4936d9fbae39bdee6018ac5b27b1c
vivek.agrawal <vivek.agrawal@metricstream.com>
2016-06-09 23:59:04 +0530

    Testing

--- src/main/java/org/skii/algorithm/SkiingSingapore.java
+++ src/main/java/org/skii/algorithm/SkiingSingapore.java
@@ -1,6 +1,7 @@
-package com.hakerearth.problems;
+package org.skii.algorithm;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import java.util.List;
 
 /**
  * 
- * The algorith to find the maximum skiing distance
+ * The algorithm to find the maximum skiing distance
  * Sample Input
  *  4 4 
 	4 8 7 3 
@@ -29,14 +30,29 @@ public class SkiingSingapore {
 		String[] line=br.readLine().split(" ");
 		int a=Integer.parseInt(line[0]);
 		int b=Integer.parseInt(line[1]);
-		input=new Integer[a][b];
-		for(int i=0;i<a;i++){
-			line=br.readLine().split(" ");
-			for(int j=0;j<line.length;j++){
-				input[i][j]=Integer.parseInt(line[j]);
-			}
+		/** used to get the input**/
+		getInput(br, a, b);
+		
+		List<Integer> output = skiingAlgorithm(a, b,input);
+		
+		/** printing the result */
+		if(output!=null){
+			for(int i=output.size()-1;i>=0;i--)
+					System.out.print(output.get(i)+" ");
 		}
-		length=input.length;
+		
+	}
+
+
+
+	/**
+	 * Skiing algorithm.
+	 *  It takes the input and gives the result in List<Integer>
+	 * @param a the a
+	 * @param b the b
+	 * @return the list
+	 */
+	public static List<Integer> skiingAlgorithm(int a, int b,Integer[][] input) {
 		List<Integer> list=new ArrayList<Integer>();
 		List<List<Integer>> superList=new ArrayList<List<Integer>>();
 		for(int i=0;i<a;i++){
@@ -46,13 +62,14 @@ public class SkiingSingapore {
 			}
 		}
 		
+		//sorting the list of results in descending order
 		Collections.sort(superList, new Comparator<List<Integer>>() {
 		  public int compare(List<Integer> o1, List<Integer> o2)  {
 		    return o2.size()-o1.size();
 		  }
 		});
 		
-		int maxSize=superList.get(0).size(); //5 in this case
+		int maxSize=superList.get(0).size(); //the maximum length
 		int maxDiff=0;
 		List<Integer>  output = null;
 		for(int z=0;z<superList.size();z++){
@@ -66,16 +83,39 @@ public class SkiingSingapore {
 			} 
 			
 		}
-		
-		if(output!=null){
-			for(int i=output.size()-1;i>=0;i--)
-					System.out.print(output.get(i)+" ");
+		return output;
+	}
+
+
+
+	/**
+	 * Gets the input.
+	 * This method is to get the input from the user
+	 * @param br the br
+	 * @param a the a
+	 * @param b the b
+	 * @return the input
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	private static void getInput(BufferedReader br, int a, int b) throws IOException {
+		input=new Integer[a][b];
+		for(int i=0;i<a;i++){
+			String[] line=br.readLine().split(" ");
+			for(int j=0;j<line.length;j++){
+				input[i][j]=Integer.parseInt(line[j]);
+			}
 		}
-		
+		length=input.length;
 	}
 	
 	
-	
+	/**
+	 * A recursive function which will find the list of all possible routes from a point
+	 * @param x
+	 * @param i
+	 * @param j
+	 * @return
+	 */
 	private static List<Integer> recursion(int x,int i,int j) {
 			List<Integer> listA=new ArrayList<Integer>();
 			List<Integer> listB=new ArrayList<Integer>();
@@ -96,10 +136,21 @@ public class SkiingSingapore {
 				listD=recursion(input[i][j+1],i,j+1);
 			}
 			
-			return greatestList(listA,listB,listC,listD,x);
+			return getLongestPath(listA,listB,listC,listD,x);
 	}
 
-	private static List<Integer> greatestList(List<Integer> listA, List<Integer> listB, List<Integer> listC,
+	
+	/**
+	 * Gets the longest path.
+	 * This will get the longest path for an element and add the current element to it.
+	 * @param listA the list a
+	 * @param listB the list b
+	 * @param listC the list c
+	 * @param listD the list d
+	 * @param x the x
+	 * @return the longest path
+	 */
+	private static List<Integer> getLongestPath(List<Integer> listA, List<Integer> listB, List<Integer> listC,
 			List<Integer> listD,int x) {
 			if(listA.size()==0 && listB.size()==0 && listC.size()==0 && listD.size()==0){
 				List<Integer> listX=new ArrayList<Integer>();
---
+++ src/test/java/org/skii/algorithm/SkiingSingaporeTest.java
@@ -0,0 +1,5 @@
+package org.skii.algorithm;
+
+public class SkiingSingaporeTest {
+	 
+}
